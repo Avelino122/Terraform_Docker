8688a8ae4005cc60b35a4c1134e6cc4094a46e6c
Avelino122 <thierryavel1@gmail.com>
2024-05-18 17:57:36 +0100

    create docker using Terraform

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "webserver"
+
+  ports {
+    internal = 80
+    external = 8000
+  }
+    volumes {
+    container_path = "/usr/share/nginx/html/index.html"
+    host_path      = "/home/avelino/learn-terraform-docker-container/index.html"
+    read_only      = true
+  }
+}
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Create a Docker Container using Terraform
+This repository teaches you the basics of building an Nginx server using Terraform. The `main.tf` file is configured to map your local port 8000 and deploy an `index.html` file in the Nginx server.
+
+## Commands to Run
+To get started with this setup, run the following commands in your terminal:
+
+```bash
+terraform init
+terraform plan
+terraform apply
+```
